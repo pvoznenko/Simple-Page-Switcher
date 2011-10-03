87f97946cfd55450ae6393b4472df14dbae3b381
Pavel Voznenko <p.voznenko@gmail.com>
2011-10-03 19:51:27 +0300

    new change

--- jquery.simplepagingplugin.js
+++ jquery.simplepagingplugin.js
@@ -2,6 +2,7 @@
  * jQuery SimplePaging plugin
  *
  * Copyright (c) 2011 Pavel Voznenko (p.voznenko@gmail.com)
+ * https://github.com/fosco-maestro/simplePaging-Plugin
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
@@ -10,15 +11,15 @@
 (function( $, undefined ) {
     $.fn.simplePagingPlugin = function( options ) {
         var settings = {
-            'perPage'     : 30,
-            'cssClassRow' : 'spp-block-row',
-            'cssIdLeft'   : 'spp-go-left',
-            'cssIdRight'  : 'spp-go-right',
-            'cssIdFirst'  : 'spp-go-first',
-            'cssIdLast'   : 'spp-go-last',
-            'cssIdMain'   : 'spp-block',
-            'arrowLeft'   : '←',
-            'arrowRight'  : '→'
+            'perPage'       : 5,
+            'cssClassRow'   : 'spp-block-row',
+            'cssClassLeft'  : 'spp-go-left',
+            'cssClassRight' : 'spp-go-right',
+            'cssClassFirst' : 'spp-go-first',
+            'cssClassLast'  : 'spp-go-last',
+            'cssClassMain'  : 'spp-block',
+            'arrowLeft'     : '\u2190',
+            'arrowRight'    : '\u2192'
         },
         $this = $( this );
                         
@@ -66,16 +67,16 @@
             methods.showRows( );
 
             $this.append( '\
-                <div id="' + settings.cssIdMain + '">\n\
-                    <a href="#go-to-first" id="' + settings.cssIdFirst + '">1</a> \n\
-                    <a href="#go-left" id="' + settings.cssIdLeft + '">' + settings.arrowLeft + '</a> \n\
+                <div class="' + settings.cssClassMain + '">\n\
+                    <a href="#go-to-first" class="' + settings.cssClassFirst + '">1</a> \n\
+                    <a href="#go-left" class="' + settings.cssClassLeft + '">' + settings.arrowLeft + '</a> \n\
                     <span>1</span> \n\
-                    <a href="#go-right" id="' + settings.cssIdRight + '">' + settings.arrowRight + '</a> \n\
-                    <a href="#go-to-last" id="' + settings.cssIdLast + '">' + pages + '</a>\n\
+                    <a href="#go-right" class="' + settings.cssClassRight + '">' + settings.arrowRight + '</a> \n\
+                    <a href="#go-to-last" class="' + settings.cssClassLast + '">' + pages + '</a>\n\
                 </div>\n\
             ' );
 
-            $( '#' + settings.cssIdFirst ).click(function() {
+            $( '.' + settings.cssClassFirst, $this ).click(function() {
                 var options = {
                     'from' : 0,
                     'to'   : settings.perPage
@@ -83,15 +84,15 @@
                                 
                 methods.showRows( options );
                             
-                $( '#' + settings.cssIdMain + ' span' ).show().text( '1' );
-                $( '#' + settings.cssIdFirst + ', #' + settings.cssIdLeft ).hide();
-                $( '#' + settings.cssIdLast + ', #' + settings.cssIdRight ).show();
+                $( '.' + settings.cssClassMain + ' span', $this ).show().text( '1' );
+                $( '.' + settings.cssClassFirst + ', .' + settings.cssClassLeft, $this ).hide();
+                $( '.' + settings.cssClassLast + ', .' + settings.cssClassRight, $this ).show();
                             
                 return false;
             })
             .hide();
 
-            $( '#' + settings.cssIdLast ).click(function() {
+            $( '.' + settings.cssClassLast, $this ).click(function() {
                 var options = { 
                     'from' : (pages * settings.perPage - elCount == 0 ? elCount - settings.perPage : (pages - 1) * settings.perPage),
                     'to'   : elCount
@@ -99,49 +100,49 @@
                                 
                 methods.showRows( options );
                             
-                $( '#' + settings.cssIdMain + ' span' ).show().text( pages );
-                $( '#' + settings.cssIdFirst + ', #' + settings.cssIdLeft ).show();
-                $( '#' + settings.cssIdLast + ', #' + settings.cssIdRight ).hide();
+                $( '.' + settings.cssClassMain + ' span', $this ).show().text( pages );
+                $( '.' + settings.cssClassFirst + ', .' + settings.cssClassLeft, $this ).show();
+                $( '.' + settings.cssClassLast + ', .' + settings.cssClassRight, $this ).hide();
                             
                 return false;
             });
 
-            $( '#' + settings.cssIdLeft ).click(function() {
-                var currentPage = parseInt( $( '#' + settings.cssIdMain + ' span' ).text() ),
-                newPage     = currentPage - 1 > 1 ? currentPage - 1 : 1,
-                options     = {
-                    'from' : (newPage == 1 ? 0 : (newPage - 1) * settings.perPage),
-                    'to'   : (newPage * settings.perPage >= elCount ? elCount : newPage * settings.perPage)
-                };
+            $( '.' + settings.cssClassLeft, $this ).click(function() {
+                var currentPage = parseInt( $( '.' + settings.cssClassMain + ' span', $this ).text() ),
+                    newPage     = currentPage - 1 > 1 ? currentPage - 1 : 1,
+                    options     = {
+                        'from' : (newPage == 1 ? 0 : (newPage - 1) * settings.perPage),
+                        'to'   : (newPage * settings.perPage >= elCount ? elCount : newPage * settings.perPage)
+                    };
 
                 methods.showRows( options );
 
-                $( '#' + settings.cssIdMain ).children().show();    
-                $( '#' + settings.cssIdMain + ' span' ).text( newPage );
+                $( '.' + settings.cssClassMain, $this ).children().show();    
+                $( '.' + settings.cssClassMain + ' span', $this ).text( newPage );
 
                 if ( newPage == 1 ) {
-                    $( '#' + settings.cssIdFirst + ', #' + settings.cssIdLeft ).hide();
+                    $( '.' + settings.cssClassFirst + ', .' + settings.cssClassLeft, $this ).hide();
                 }
                             
                 return false;
             })
             .hide();
 
-            $( '#' + settings.cssIdRight ).click(function() {
-                var currentPage = parseInt($('#' + settings.cssIdMain + ' span').text()),
-                newPage     = currentPage + 1 < pages ? currentPage + 1 : pages,
-                options = {
-                    'from' : (newPage == pages ? (pages * settings.perPage - elCount == 0 ? elCount - settings.perPage : (pages - 1) * settings.perPage) : currentPage * settings.perPage),
-                    'to'   : (newPage * settings.perPage >= elCount ? elCount : newPage * settings.perPage)
-                };
-
+            $( '.' + settings.cssClassRight, $this ).click(function() {
+                var currentPage = parseInt($('.' + settings.cssClassMain + ' span', $this ).text()),
+                    newPage     = currentPage + 1 < pages ? currentPage + 1 : pages,
+                    options     = {
+                        'from' : (newPage == pages ? (pages * settings.perPage - elCount == 0 ? elCount - settings.perPage : (pages - 1) * settings.perPage) : currentPage * settings.perPage),
+                        'to'   : (newPage * settings.perPage >= elCount ? elCount : newPage * settings.perPage)
+                    };
+                    
                 methods.showRows( options );
 
-                $( '#' + settings.cssIdMain ).children().show();    
-                $( '#' + settings.cssIdMain + ' span' ).text( newPage );
+                $( '.' + settings.cssClassMain, $this ).children().show();    
+                $( '.' + settings.cssClassMain + ' span', $this ).text( newPage );
 
                 if ( newPage == pages ) {
-                    $( '#' + settings.cssIdLast + ', #' + settings.cssIdRight ).hide();
+                    $( '.' + settings.cssClassLast + ', .' + settings.cssClassRight, $this ).hide();
                 }
                             
                 return false;
